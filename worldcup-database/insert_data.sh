5b3fd99a1e6878547793a01d6ca1b9a37fc6aabe
Daniel Keicher <dnlkch86@gmail.com>
2023-02-03 15:21:53 +0100

    completed insert data script

--- worldcup-database/insert_data.sh
+++ worldcup-database/insert_data.sh
@@ -12,19 +12,30 @@ echo "$($PSQL "TRUNCATE games, teams;")"
 
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
-  if [[ $WINNER != "winner" ]]
+  if [[ $YEAR != "year" ]]
   then
     # insert winner in teams if not found
     WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER';")
     if [[ -z $WINNER_ID ]]
     then
       echo "$($PSQL "INSERT INTO teams(name) VALUES('$WINNER');")"
+      # get newly inserted winner_id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER';")
     fi
     # insert opponent in teams if not found
     OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT';")
     if [[ -z $OPPONENT_ID ]]
     then
       echo "$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT');")"
+      # get newly inserted opponent_id
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT';")
+    fi
+    # check if current game is already inserted in games
+    GAME_ID=$($PSQL "SELECT game_id FROM games WHERE year='$YEAR' AND round='$ROUND' AND winner_id='$WINNER_ID' AND opponent_id='$OPPONENT_ID';")
+    if [[ -z $GAME_ID ]]
+    then
+      # insert the game in games
+      echo "$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS);")"
     fi
   fi
 done
