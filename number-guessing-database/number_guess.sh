75a4762a31fb28a0e5c44c3c57a827f84e14ed8e
Daniel Keicher <dnlkch86@gmail.com>
2023-02-25 14:10:17 +0100

    feat: number guessing game, still in progress

--- number-guessing-database/number_guess.sh
+++ number-guessing-database/number_guess.sh
@@ -20,6 +20,31 @@ else
   echo "Welcome, $USERNAME! It looks like this is your first time here."
 fi
 
-GENERATE_RANDOM_NUMBER() {
-  NUMBER=$(( RANDOM % 1000 + 1))
-}
+NUMBER=$(( RANDOM % 1000 + 1))
+echo $NUMBER
+
+echo "Guess the secret number between 1 and 1000:"  
+read USER_GUESS
+COUNT=1
+game_on=true
+while [ game_on ]
+do
+  if [[ ! $USER_GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  else
+    if [[ $USER_GUESS -eq $NUMBER ]]
+    then
+      echo "You guessed it in $COUNT tries. The secret number was $NUMBER. Nice job!"
+      break
+    else if [[ $USER_GUESS -lt $NUMBER ]]
+      echo "It's higher than that, guess again:"
+    else
+      echo "It's lower than that, guess again:"
+    fi
+  fi
+  
+  ((COUNT++))
+  read USER_GUESS
+done
+
