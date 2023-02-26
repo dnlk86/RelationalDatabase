76672e4578a850beba516eed117c88bfa0137a6c
Daniel Keicher <dnlkch86@gmail.com>
2023-02-26 11:10:11 +0100

    final version od number_guess script

--- number-guessing-database/number_guess.sh
+++ number-guessing-database/number_guess.sh
@@ -5,8 +5,8 @@ echo "Enter your username:"
 read USERNAME
 
 # search fo user in the db
-LOGIN_RESULT=$($PSQL "SELECT games_played, best_game FROM players WHERE user_name='$USERNAME';")
-if [[ -z $LOGIN_RESULT ]]
+USER_ID=$($PSQL "SELECT user_id FROM players WHERE user_name='$USERNAME';")
+if [[ -z $USER_ID ]]
 then 
   # enter the new user in the database
   USER_INSERT_RESULT=$($PSQL "INSERT INTO players(user_name) VALUES('$USERNAME');")
@@ -16,50 +16,44 @@ then
   BEST_GAME=0
 else
   # greet an old user coming back
-  echo "$LOGIN_RESULT" | while IFS='|' read GAMES_PLAYED BEST_GAME
-  do
-    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
-  done
+  GAMES_PLAYED=$($PSQL "SELECT games_played FROM players WHERE user_id=$USER_ID;")
+  BEST_GAME=$($PSQL "SELECT best_game FROM players WHERE user_id=$USER_ID;")
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
 NUMBER=$(( RANDOM % 1000 + 1))
-echo $NUMBER
-
-echo "Guess the secret number between 1 and 1000:"  
-read USER_GUESS
-COUNT=1
-
-# increment user's number of games
-
+  
 game_on=true
-while [ $game_on ]
+echo "Guess the secret number between 1 and 1000:"
+while [ "$game_on" == "true" ]
 do
+  ((COUNT++))
+  read USER_GUESS
+
   if [[ ! $USER_GUESS =~ ^[0-9]+$ ]]
   then
     echo "That is not an integer, guess again:"
   else
-    if [[ $USER_GUESS -eq $NUMBER ]]
+    if [[ ! $USER_GUESS -eq $NUMBER ]]
     then
+      if [[ $USER_GUESS -lt $NUMBER ]]
+      then
+        echo "It's higher than that, guess again:"
+      else
+        echo "It's lower than that, guess again:"
+      fi
+    else
       echo "You guessed it in $COUNT tries. The secret number was $NUMBER. Nice job!"
       # check if new best
-      if [[ $COUNT -lt $BEST_GAME || $GAMES_PLAYED -eq 0 ]]
+      if [[ $COUNT < $BEST_GAME || $GAMES_PLAYED == 0 ]]
       then
         # replace new best game
         BEST_GAME=$COUNT
       fi
-      # insert
       ((GAMES_PLAYED++))
       UPDATE_PLAYER_RESULT=$($PSQL "UPDATE players SET games_played=$GAMES_PLAYED, best_game=$BEST_GAME WHERE user_name='$USERNAME';")
-      break
-    elif [[ $USER_GUESS -lt $NUMBER ]]
-    then
-      echo "It's higher than that, guess again:"
-    else
-      echo "It's lower than that, guess again:"
+      game_on=false
     fi
   fi
-  
-  ((COUNT++))
-  read USER_GUESS
 done
 
